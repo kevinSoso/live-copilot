2342debb2a6a6026c3fb2ab7d1db9611af17376b
kevinzhang <yafengzhang@yonghongtech.com>
2025-03-21 23:44:19 +0800

    add chatbot

--- nlp/ChatCleaner.py
+++ nlp/ChatCleaner.py
@@ -5,8 +5,8 @@ from cleantext import clean
 
 # 去除文本中的 emoji、标点符号、重复字符等
 def clean_comment(text):
-    text = text.lower()  # 统一小写
-    text = emoji.replace_emoji(text, replace='')  # 去除 emoji
+    text = text.lower() 
+    text = emoji.replace_emoji(text, replace='')  
     text = "".join(c for c in text if unicodedata.category(c)[0] not in ["S", "C"])  # 去掉特殊符号
     text = re.sub(r"(.)\1{2,}", r"\1\1", text)  # 限制重复字符
     text = re.sub(r"[^\w\s]", "", text)  # 去除标点符号
